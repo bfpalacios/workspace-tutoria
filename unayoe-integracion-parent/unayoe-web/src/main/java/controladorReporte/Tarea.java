9f2fe4f6f4fe28e6b76371ef186871fe82561af6
JEANPIERE <JEANPIERE@JEANPIERE-PC>
2016-06-11 10:14:42 -0500

    avance reportes

---
+++ unayoe-integracion-parent/unayoe-web/src/main/java/controladorReporte/Tarea.java
@@ -0,0 +1,69 @@
+package controladorReporte;
+
+public class Tarea {
+
+	private String sesion;
+	private String razon;
+	private String tarea;
+	private String criticidad;
+	private String fechaInicio;
+	private String fechaFin;
+	private String fechaCumplida;
+	private String estado;
+	public Tarea() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getSesion() {
+		return sesion;
+	}
+	public void setSesion(String sesion) {
+		this.sesion = sesion;
+	}
+	public String getRazon() {
+		return razon;
+	}
+	public void setRazon(String razon) {
+		this.razon = razon;
+	}
+	public String getTarea() {
+		return tarea;
+	}
+	public void setTarea(String tarea) {
+		this.tarea = tarea;
+	}
+
+	public String getCriticidad() {
+		return criticidad;
+	}
+	public void setCriticidad(String criticidad) {
+		this.criticidad = criticidad;
+	}
+	public String getFechaInicio() {
+		return fechaInicio;
+	}
+	public void setFechaInicio(String fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+	public String getFechaFin() {
+		return fechaFin;
+	}
+	public void setFechaFin(String fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+	public String getFechaCumplida() {
+		return fechaCumplida;
+	}
+	public void setFechaCumplida(String fechaCumplida) {
+		this.fechaCumplida = fechaCumplida;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
+	
+}
