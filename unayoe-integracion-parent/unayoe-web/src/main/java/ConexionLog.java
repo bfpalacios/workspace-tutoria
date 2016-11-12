56bf5a8a0bc55433a51dd93b63fb0b4e0694d094
Anonymousxd123 <cristian12dn@gmail.com>
2016-11-12 10:39:42 -0500

    Añadido el Componente log

---
+++ unayoe-integracion-parent/unayoe-web/src/main/java/ConexionLog.java
@@ -0,0 +1,57 @@
+
+import java.net.URL;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class ConexionLog {
+	
+	
+	static Logger log=Logger.getLogger(ConexionLog.class);
+    static URL url=ConexionLog.class.getResource("log4j.properties");
+    
+    public void conectar(int num) {
+        PropertyConfigurator.configure(url);
+        if(num==0){
+            //conexion fue exitosa
+            log.info("La conexion fue exitosa");
+        }else if(num<0){
+            //warning, un error puede suceder durante la ejecucion
+            log.warn("Posiblemente suceda un Error");
+        }else{
+            // error fatal
+            log.fatal("Ocurrio un error fatal");
+        }
+    }
+    
+    /**
+     * Metodo para registrar un mensaje en el Log
+     * Seleccione el tipo de error
+     * - 0: Mensaje informativo
+     * - 1: Mensaje de Advertencia
+     * - 2: Mensaje de Error
+     * - 4: Mensaje de Error fatal
+     * @param numero
+     * @param mensaje
+     */
+    public static void registrarMensaje(int numero, String mensaje){
+        
+        PropertyConfigurator.configure(url);
+        switch(numero){
+            case 0: //Mensaje informativo
+                log.info(mensaje);
+            case 1: //Mensaje de Warning
+                log.warn(mensaje);
+            case 2: //Mensaje de Warning
+                log.warn(mensaje);
+            case 3: //Mensaje de error
+                log.error(mensaje);
+            case 4:
+                //Mensaje de error fatal
+                log.fatal(mensaje);
+            default: System.out.println("Error el numero indicado es incorrecto");
+        }
+    }
+   
+
+
+}
