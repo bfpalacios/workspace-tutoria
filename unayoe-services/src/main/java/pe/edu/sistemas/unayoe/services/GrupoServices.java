6c11dab78291199e26cd1444268947a7f3ac655f
flucasdlc <freddy711.12@gmail.com>
2016-11-25 14:35:13 -0500

    JavaDOC

--- unayoe-services/src/main/java/pe/edu/sistemas/unayoe/services/GrupoServices.java
+++ unayoe-services/src/main/java/pe/edu/sistemas/unayoe/services/GrupoServices.java
@@ -6,6 +6,12 @@ import pe.edu.sistemas.unayoe.unayoe.bo.AlumnoBO;
 import pe.edu.sistemas.unayoe.unayoe.bo.CursoBO;
 import pe.edu.sistemas.unayoe.unayoe.bo.GrupoBO;
 
+/**
+ * Interfaz Servicios de Grupos
+ * @author Bruno Palacios
+ *
+ */
+
 public interface GrupoServices {
 	
 	public void  guardarGrupos(List<GrupoBO> lista) throws Exception;
