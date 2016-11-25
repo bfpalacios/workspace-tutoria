531e58ffbc3add0edb7d7364cbb05ee1d4202919
flucasdlc <freddy711.12@gmail.com>
2016-11-25 14:43:16 -0500

    JavaDOC

--- unayoe-services/src/main/java/pe/edu/sistemas/unayoe/services/UsuarioServiceSecurity.java
+++ unayoe-services/src/main/java/pe/edu/sistemas/unayoe/services/UsuarioServiceSecurity.java
@@ -1,7 +1,11 @@
 package pe.edu.sistemas.unayoe.services;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
-
+/**
+ * Interfaz Servicio de Seguridad de Usuario
+ * @author Bruno Palacios
+ *
+ */
 public interface UsuarioServiceSecurity extends UserDetailsService{
 
 }
