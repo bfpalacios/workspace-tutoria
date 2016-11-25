3ec54ec3083c835fc504e18a8fb475176d8107fd
flucasdlc <freddy711.12@gmail.com>
2016-11-25 14:36:50 -0500

    JavaDOC

--- unayoe-services/src/main/java/pe/edu/sistemas/unayoe/services/PeriodoServices.java
+++ unayoe-services/src/main/java/pe/edu/sistemas/unayoe/services/PeriodoServices.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 import pe.edu.sistemas.unayoe.unayoe.bo.PeriodoBO;
 
+/**
+ * Interfaz Servicios de Periodo
+ * @author Bruno Palacios
+ *
+ */
+
 public interface PeriodoServices {
 
 	public List<PeriodoBO> getPeriodos();
