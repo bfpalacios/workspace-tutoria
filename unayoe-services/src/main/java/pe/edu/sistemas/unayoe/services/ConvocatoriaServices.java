06d8541f8b5ed1bf6a6ecc0f0e0b5998739022b9
flucasdlc <freddy711.12@gmail.com>
2016-11-25 14:32:51 -0500

    Update ConvocatoriaServices.java

--- unayoe-services/src/main/java/pe/edu/sistemas/unayoe/services/ConvocatoriaServices.java
+++ unayoe-services/src/main/java/pe/edu/sistemas/unayoe/services/ConvocatoriaServices.java
@@ -3,6 +3,11 @@ package pe.edu.sistemas.unayoe.services;
 import java.util.List;
 
 import pe.edu.sistemas.unayoe.unayoe.bo.ConvocatoriaBO;
+/**
+ * Iterfaz Convocatoria
+ * @author Bruno Palacios
+ *
+ */
 
 public interface ConvocatoriaServices {
 
